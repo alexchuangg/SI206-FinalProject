4feec27f428b441f9cb6477af1409548a7d4b72c
Alex Chuang <72578355+konneech@users.noreply.github.com>
2022-12-09 12:19:49 -0800

    oysg

--- airquality.py
+++ airquality.py
@@ -8,11 +8,14 @@ API_Key = '6aa30181-6bb9-4f96-a17a-c6839fe06967'
 
 def get_data(city):
     url = f'https://api.api-ninjas.com/v1/airquality?city={city}'
+    try: 
+        resp = requests.get(url, headers={'X-Api-Key': '6aa30181-6bb9-4f96-a17a-c6839fe06967'})
+        data = json.loads(resp.text)
+        print(data)
+        return data
 
-    response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
-    else:
-        print("Error:", response.status_code, response.text)
+    except: 
+        print('Exception')
+        return None
 
 get_data("Ann Arbor")
