6d20126943bc3c21f9ae6d00a6c53a91f3d4c098
Alex Chuang <72578355+konneech@users.noreply.github.com>
2022-12-09 12:17:06 -0800

    osyb

--- airquality.py
+++ airquality.py
@@ -7,7 +7,7 @@ import requests
 API_Key = '6aa30181-6bb9-4f96-a17a-c6839fe06967'
 
 def get_data(city):
-    url = f'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+    url = f'https://api.api-ninjas.com/v1/airquality?city={city}'
 
     response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
     if response.status_code == requests.codes.ok:
