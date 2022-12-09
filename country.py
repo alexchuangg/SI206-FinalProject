73a8298ec0cac262b027f2bae6d48cbadf7bc134
Alex Chuang <72578355+konneech@users.noreply.github.com>
2022-12-08 23:36:41 -0800

    flawed database

--- country.py
+++ country.py
@@ -6,8 +6,9 @@ import requests
 
 API_Key = '6aa30181-6bb9-4f96-a17a-c6839fe06967'
 
-def get_data():
-    url = f'https://api.api-ninjas.com/v1/country?name='
+def get_data(country):
+
+    url = f'https://api.api-ninjas.com/v1/country?name={country}'
 
     try: 
         resp = requests.get(url, headers={'X-Api-Key': 'op+jNXkyR4hqbDr/7s+5yg==d1NJZb0OzNsLkset'})
@@ -30,16 +31,16 @@ def create_population_table(data, cur, conn):
     internet_users = []
     
     for country in data:
-        countries.append(country[1]['name'])
-        population.append(country[1]['population'])
-        female_life_expectancy.append(country[1]["life_expectancy_female"])
-        male_life_expectancy.append(country[0]["life_expectancy_male"])
-        internet_users.append(countries[1]['internet_users'])
+        countries.append(country['name'])
+        population.append(country['population'])
+        female_life_expectancy.append(country["life_expectancy_female"])
+        male_life_expectancy.append(country["life_expectancy_male"])
+        internet_users.append(country['internet_users'])
 
-    cur.execute("CREATE TABLE IF NOT EXISTS Population (country TEXT PRIMARY KEY, population INTEGER, male_life_expectancy INTEGER, internet_users INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS populations3 (country TEXT PRIMARY KEY, population INTEGER, male_life_expectancy INTEGER, female_life_expectancy INTEGER, internet_users INTEGER, gap_expectancies INTEGER)")
 
     for i in range(len(countries)):
-        cur.execute("INSERT INTO Population (country,population,male_life_expectancy, female_life_expectancy, internet_users) VALUES (?,?,?,?,?)",(countries[i],population[i],male_life_expectancy[i],internet_users[i]))
+        cur.execute("INSERT OR IGNORE INTO populations3 (country,population,male_life_expectancy,female_life_expectancy,internet_users) VALUES (?,?,?,?,?)",(countries[i],population[i],male_life_expectancy[i],female_life_expectancy[i],internet_users[i]))
     conn.commit()
 
     # calculate the gap between female and male life expectancies
@@ -48,7 +49,7 @@ def create_population_table(data, cur, conn):
         gap_expectancies.append(male_life_expectancy[i] - female_life_expectancy[i])
 
     for i in range(len(countries)):
-        cur.execute("UPDATE Population SET gap_expectancies = ? WHERE country = ?", (gap_expectancies[i], countries[i]))
+        cur.execute("UPDATE populations2 SET gap_expectancies = ? WHERE country = ?", (gap_expectancies[i], countries[i]))
     conn.commit()
 
 
@@ -57,8 +58,12 @@ def main():
     conn = sqlite3.connect(path+'/country.db')
     cur = conn.cursor()
 
-    data = get_data()
-    create_population_table(data, cur, conn)
+
+    countries = ['Afghanistan', 'Albania', 'Algeria', 'Angola', 'Antarctica', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia', 'Botswana', 'Brazil', 'Bulgaria', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, United States', 'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe']
+    for i in range(len(countries)):
+        data = get_data(countries[i])
+        create_population_table(data, cur, conn)
+    
 
 if __name__ == "__main__":
     main()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-data['name']
+    for country in data:
+        countries.append(country[1]['name'])
+        population.append(country[1]['population'])
+        female_life_expectancy.append(country[1]["life_expectancy_female"])
+        male_life_expectancy.append(country[0]["life_expectancy_male"])
+        internet_users.append(countries[1]['internet_users'])
